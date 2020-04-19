1e067c89d42897e4e9394c3f1028faa33bdc69d6
Abel Babini Filho <abelbfilho@hotmail.com>
2020-04-19 15:37:29 -0300

    Falta ajustar importação do CSV

--- src/services/ImportTransactionsService.ts
+++ src/services/ImportTransactionsService.ts
@@ -1,8 +1,66 @@
-import Transaction from '../models/Transaction';
+import path from 'path';
+import fs from 'fs';
+import csv from 'csv-parse';
 
+import Transaction from '../models/Transaction';
+import uploadConfig from '../config/upload';
+import CreateTransactionService from './CreateTransactionService';
+
+interface Request {
+  csvFile: string;
+}
+
+interface CsvDto {
+  title: string;
+  type: 'income' | 'outcome';
+  value: number;
+  category: string;
+}
+interface TransactionDTO {
+  id: string;
+  title: string;
+  type: 'income' | 'outcome';
+  value: number;
+  category_id: string;
+}
+
+const readCsv = (pathCSV: string): Promise<CsvDto[]> =>
+  new Promise(resolve => {
+    const csvData: CsvDto[] = [];
+    const csvStream = fs
+      .createReadStream(pathCSV)
+      .pipe(csv({ columns: true, ltrim: true }))
+      .on('data', async data => {
+        csvStream.pause();
+        if (data[0] !== 'title' && data[1] !== 'type' && data[2] !== 'value') {
+          csvData.push(data);
+          csvStream.resume();
+        } else {
+          csvStream.resume();
+        }
+      })
+      .on('end', () => {
+        resolve(csvData);
+      });
+  });
 class ImportTransactionsService {
-  async execute(): Promise<Transaction[]> {
-    // TODO
+  async execute({ csvFile }: Request): Promise<any> {
+    const pathCSV = path.join(uploadConfig.directory, csvFile);
+    const createTransaction = new CreateTransactionService();
+    const transactions: Transaction[] = [];
+
+    const contentCsv = await readCsv(pathCSV);
+
+    await Promise.all(
+      contentCsv.map(async row => {
+        const transaction = await createTransaction.execute(row);
+        transactions.push(transaction);
+      }),
+    );
+
+    await fs.promises.unlink(pathCSV);
+
+    return transactions;
   }
 }
 
